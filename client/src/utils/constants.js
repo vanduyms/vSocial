492b70ef54b6193efa77bc3137d46b596a586491
vanduyms <vanduyms@gmail.com>
2024-06-04 13:55:48 +0700

    update host backend

---
+++ client/src/utils/constants.js
@@ -0,0 +1,1 @@
+export const host = "https://vsocial-backend.up.railway.app"
--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -1,42 +1,43 @@
 import axios from "axios";
+import { host } from "./constants";
 
 export const getDataAPI = async (url, token) => {
-  const res = axios.get(`http://localhost:3001/api/${url}`, {
+  const res = axios.get(`${host}/api/${url}`, {
     headers: { Authorization: token }
   });
   return res;
 }
 
 export const deleteDataAPI = async (url, token) => {
-  const res = axios.delete(`http://localhost:3001/api/${url}`, {
+  const res = axios.delete(`${host}/api/${url}`, {
     headers: { Authorization: token }
   });
   return res;
 }
 
 export const postDataAPI = async (url, post, token) => {
-  const res = axios.post(`http://localhost:3001/api/${url}`, post, {
+  const res = axios.post(`${host}/api/${url}`, post, {
     headers: { Authorization: token }
   });
   return res;
 }
 
 export const putDataAPI = async (url, post, token) => {
-  const res = axios.put(`http://localhost:3001/api/${url}`, post, {
+  const res = axios.put(`${host}/api/${url}`, post, {
     headers: { Authorization: token }
   });
   return res;
 }
 
 export const patchDataAPI = async (url, post, token) => {
-  const res = axios.patch(`http://localhost:3001/api/${url}`, post, {
+  const res = axios.patch(`${host}/api/${url}`, post, {
     headers: { Authorization: token }
   });
   return res;
 }
 
 export const postAPI = async (url, token) => {
-  const res = axios.post(`http://localhost:3001/api/${url}`, {
+  const res = axios.post(`${host}/api/${url}`, {
     headers: { Authorization: token }
   });
   return res;
